1570a24d483ea0d34fa9f8c5feff83917b617b95
Irina <kis.93@mail.ru>
2024-10-23 20:32:34 +0300

    add setupLogger

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -2,18 +2,46 @@ package main
 
 import (
 	"github.com/aventhis/go-url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	// init config
 	cfg := config.MustLoad()
-	//fmt.Println(cfg)
-	// TODO: init logger: slog
 
+	//  init logger
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 	// TODO: init storage: sqlite
 
 	// TODO: init router: chi, "chi render"
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
